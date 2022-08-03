13f1d9ab9fb5727c21a2fdcc7461afab111f53cc
adityasinha198 <adityasinha198@gmail.com>
2022-08-04 01:11:40 +0530

    task6

---
+++ task6.js
@@ -0,0 +1,4 @@
+const s = document.querySelector(".list-group-item:nth-child(2)")
+s.style.backgroundColor = "green"
+const p = document.querySelector(".list-group-item:nth-child(3)")
+p.style.visibility = "hidden"
